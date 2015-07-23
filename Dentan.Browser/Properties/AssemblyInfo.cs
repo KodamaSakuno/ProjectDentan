b11e4e984805afc1d3b49de07b92dfba95ee8a10
神樹桜乃 <kodama@sakuno.moe>
2015-07-23 18:03:49 +0800

    Version 0.0.1.4

--- Dentan.Browser/Properties/AssemblyInfo.cs
+++ Dentan.Browser/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.3")]
-[assembly: AssemblyFileVersion("0.0.1.3")]
+[assembly: AssemblyVersion("0.0.1.4")]
+[assembly: AssemblyFileVersion("0.0.1.4")]
