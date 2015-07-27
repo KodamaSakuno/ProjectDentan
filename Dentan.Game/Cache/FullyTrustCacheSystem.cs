507b480440415b5f54d12da76145d781cc8b8d33
神樹桜乃 <kodama@sakuno.moe>
2015-07-27 21:41:05 +0800

    + Fully trust cache system

---
+++ Dentan.Game/Cache/FullyTrustCacheSystem.cs
@@ -0,0 +1,39 @@
+﻿using Fiddler;
+using Moen.KanColle.Dentan.Proxy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moen.KanColle.Dentan.Cache
+{
+    class FullyTrustCacheSystem : ICacheSystem
+    {
+        public void ProcessRequest(ResourceSession rpSession)
+        {
+            var rPath = rpSession.Url;
+            var rQueryPosition = rPath.LastIndexOf('?');
+            if (rQueryPosition != -1)
+                rPath = rPath.Remove(rQueryPosition);
+            
+            var rFilename = Path.Combine(ResourceCache.CacheFolder, rPath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar));
+            var rFileInfo = new FileInfo(rFilename);
+            if (!rFileInfo.Exists)
+                return;
+            
+            var rFiddlerSession = rpSession.FiddlerSession;
+            rFiddlerSession.utilCreateResponseAndBypassServer();
+            rFiddlerSession.ResponseBody = File.ReadAllBytes(rFilename);
+            rFiddlerSession.oResponse.headers.Add("Last-Modified", rFileInfo.LastWriteTime.ToString("s"));
+
+            rpSession.ContentLength = rpSession.LoadedBytes = rFiddlerSession.ResponseBody.Length;
+            rpSession.Status = SessionStatus.LoadedFromCache;
+        }
+
+        public void ProcessResponse(ResourceSession rpSession)
+        {
+        }
+    }
+}
