7de2726756fe517b8088c7e52707d9caa2f5ce7d
神樹桜乃 <kodama@sakuno.moe>
2015-08-08 13:18:12 +0800

    + Shortcut key support for menu

--- Dentan/ViewModel/Menu/MenuItemViewModel.cs
+++ Dentan/ViewModel/Menu/MenuItemViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Moen.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Windows.Input;
 
 namespace Moen.KanColle.Dentan.ViewModel.Menu
@@ -46,15 +48,47 @@ namespace Moen.KanColle.Dentan.ViewModel.Menu
             }
         }
 
+        KeyGesture r_KeyGesture;
+        public KeyGesture KeyGesture
+        {
+            get { return r_KeyGesture; }
+            set
+            {
+                if (r_KeyGesture != value)
+                {
+                    r_KeyGesture = value;
+                    InputGestureText = r_KeyGesture == null ? null : r_KeyGesture.GetDisplayStringForCulture(CultureInfo.CurrentCulture);
+
+                    OnPropertyChanged(nameof(KeyGesture));
+                }
+            }
+        }
+        string r_InputGestureText;
+        public string InputGestureText
+        {
+            get { return r_InputGestureText; }
+            private set
+            {
+                if (r_InputGestureText != value)
+                {
+                    r_InputGestureText = value;
+                    OnPropertyChanged(nameof(InputGestureText));
+                }
+            }
+        }
+
         ObservableRangeCollection<object> r_Items;
         public ObservableRangeCollection<object> Items { get { return r_Items; } }
 
         public MenuItemViewModel(string rpText)
             : this(rpText, null) { }
         public MenuItemViewModel(string rpText, ICommand rpCommand)
+            : this(rpText, rpCommand, null) { }
+        public MenuItemViewModel(string rpText, ICommand rpCommand, KeyGesture rpKeyGesture)
         {
             Title = rpText;
             Command = rpCommand;
+            KeyGesture = rpKeyGesture;
 
             r_Items = new ObservableRangeCollection<object>();
 
@@ -67,5 +101,14 @@ namespace Moen.KanColle.Dentan.ViewModel.Menu
         {
             return null;
         }
+
+        public void KeyBinding(InputBindingCollection rpBindings)
+        {
+            KeyBindingCore(rpBindings);
+
+            foreach (var rMenu in Items.OfType<MenuItemViewModel>())
+                rMenu.KeyBinding(rpBindings);
+        }
+        protected virtual void KeyBindingCore(InputBindingCollection rpBindings) { }
     }
 }
