672839b5cbd335c8868fb1f5b6ff972a90b10dae
神樹桜乃 <kodama@sakuno.moe>
2015-08-11 17:13:41 +0800

    + Air battle of combined fleet

--- Dentan.Game/Api/Parser/Battle/DayCombinedFleetCTFBattleParser.cs
+++ Dentan.Game/Api/Parser/Battle/DayCombinedFleetCTFBattleParser.cs
@@ -3,11 +3,13 @@
 namespace Moen.KanColle.Dentan.Api.Parser.Battle
 {
     [Api("api_req_combined_battle/battle")]
+    [Api("api_req_combined_battle/airbattle")]
     class DayCombinedFleetCTFBattleParser : DayCombinedFleetBattleParserBase
     {
         protected override void ProcessCore(RawBattle rpData)
         {
             ProcessCombinedAerial(rpData.AerialCombat);
+            ProcessCombinedAerial(rpData.AerialCombatSecondRound);
             ProcessSupportAttack(rpData.SupportInfo);
             ProcessCombinedFleetTorpedoSalvo(rpData.OpeningTorpedoSalvo);
 
